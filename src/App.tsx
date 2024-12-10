d052cf8718c100d21a6dccc1379d489e696a2443
royyan <royanhusen007@gmail.com>
2024-12-11 06:06:21 +0700

    recipe details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Browse from "./pages/Browse";
 import SearchDetails from "./pages/SearchDetails";
 import CategoryDetails from "./pages/CategoryDetails";
+import RecipeDetails from "./pages/RecipeDetails";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Browse />} />
         <Route path="/search" element={<SearchDetails />} />
+        <Route path="/recipe/:slug" element={<RecipeDetails />} />
         <Route path="/category/:slug" element={<CategoryDetails />} />
       </Routes>
     </Router>
