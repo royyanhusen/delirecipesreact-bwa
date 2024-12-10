436bca65612be9cd5b442b779fcc8696c2c9cb39
royyan <royanhusen007@gmail.com>
2024-12-10 21:54:29 +0700

    halaman details category

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Browse from "./pages/Browse";
 import SearchDetails from "./pages/SearchDetails";
+import CategoryDetails from "./pages/CategoryDetails";
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Browse />} />
-        <Route path="search" element={<SearchDetails />} />
+        <Route path="/search" element={<SearchDetails />} />
+        <Route path="/category/:slug" element={<CategoryDetails />} />
       </Routes>
     </Router>
   )
---
+++ src/wrappers/CategoryFeaturedRecipesWrapper.tsx
@@ -0,0 +1,71 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import FeaturedRecipeCard from "../components/FeaturedRecipeCard";
+import { useEffect, useState } from "react";
+import { Category } from "../types/type";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default function CategoryFeaturedRecipesWrapper() {
+
+    const { slug } = useParams<{ slug: string }>();
+    const [category, setCategory] = useState<Category | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>();
+
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8000/api/category/${slug}`)
+            .then(response => {
+                setCategory(response.data.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, [slug]);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>Error loading data: {error}</div>
+    }
+
+    if (!category) {
+        return <div>Category not found</div>
+    }
+
+    return (
+        <section id="MadeByPeople">
+            <div className="flex items-center justify-between px-5">
+                <h2 className="font-bold">Made by People</h2>
+                <a
+                    href="#"
+                    className="font-semibold text-sm leading-[21px] text-[#FF4C1C]"
+                >
+                    Explore All
+                </a>
+            </div>
+            <div className="swiper w-full mt-3">
+                <Swiper
+                    className="w-full mt-3"
+                    direction='horizontal'
+                    spaceBetween={16}
+                    slidesPerView="auto"
+                    slidesOffsetBefore={20}
+                    slidesOffsetAfter={20}
+                >
+                    {
+                        category.recipes.length > 0 ? (
+                            category.recipes.map((recipe) => (
+                                <SwiperSlide key={recipe.id} className="swiper-slide !w-fit">
+                                    <FeaturedRecipeCard recipe={recipe} />
+                                </SwiperSlide>
+                            ))) : (<p>Belum ada data recipe dari kategori tersebut </p>)
+                    }
+                </Swiper>
+            </div>
+        </section>
+    );
+}
